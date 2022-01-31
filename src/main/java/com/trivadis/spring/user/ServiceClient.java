85c80df40de96fd8cae5dfdf1282628c9c8e58c5
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-01-31 11:43:42 +0100

    Field Injection

--- src/main/java/com/trivadis/spring/user/ServiceClient.java
+++ src/main/java/com/trivadis/spring/user/ServiceClient.java
@@ -1,12 +1,26 @@
 package com.trivadis.spring.user;
 
 import com.trivadis.spring.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ServiceClient {
+import javax.annotation.PostConstruct;
 
-    public ServiceClient(UserService userService) {
+@Component
+public class ServiceClient implements CommandLineRunner {
+
+    // Field Injection
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void init() {
+        userService.getAllUsers().forEach(System.out::println);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         userService.getAllUsers().forEach(System.out::println);
     }
 }
--- src/main/java/com/trivadis/spring/user/UsermanagementApplication.java
+++ src/main/java/com/trivadis/spring/user/UsermanagementApplication.java
@@ -1,13 +1,8 @@
 package com.trivadis.spring.user;
 
-import com.trivadis.spring.user.service.UserService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 
@@ -18,8 +13,8 @@ public class UsermanagementApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(UsermanagementApplication.class, args);
 
-        UserService userService = applicationContext.getBean(UserService.class);
-        userService.getAllUsers().forEach(System.out::println);
+//        UserService userService = applicationContext.getBean(UserService.class);
+//        userService.getAllUsers().forEach(System.out::println);
     }
 
 
