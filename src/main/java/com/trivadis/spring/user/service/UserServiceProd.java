58a105896dc4b6f1f6dddf58df1cf06fc870edac
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-02-01 10:08:37 +0100

    Properties

--- src/main/java/com/trivadis/spring/user/service/UserServiceProd.java
+++ src/main/java/com/trivadis/spring/user/service/UserServiceProd.java
@@ -1,11 +1,16 @@
 package com.trivadis.spring.user.service;
 
 import com.trivadis.spring.user.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserServiceProd implements UserService {
+
+    @Value("${user.firstnames}")
+    public String[] firstnames;
 
     public UserServiceProd() {
         System.out.println("#####");
@@ -13,7 +18,6 @@ public class UserServiceProd implements UserService {
 
     @Override
     public List<User> getAllUsers() {
-        return Arrays.asList(
-                new User("PROD", "PROD"));
+        return Arrays.stream(firstnames).map(f -> new User(f, f)).collect(Collectors.toList());
     }
 }
