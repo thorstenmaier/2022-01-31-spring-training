c52fb58b18818041085665d5db13bff5165779e9
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-02-02 15:05:10 +0100

    Initial security config

--- src/main/java/com/trivadis/spring/user/UsermanagementApplication.java
+++ src/main/java/com/trivadis/spring/user/UsermanagementApplication.java
@@ -1,22 +1,13 @@
 package com.trivadis.spring.user;
 
-import com.trivadis.spring.user.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class UsermanagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsermanagementApplication.class, args);
-
-//        new RestTemplate().postForEntity("http://localhost:8080/user/", )
-
-//        User user = new RestTemplate().getForObject("http://localhost:8080/user/{id}", User.class, 1l);
-//        System.out.println(user);
     }
 
 
